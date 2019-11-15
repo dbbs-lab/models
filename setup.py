0ec6853277108c30c12b191839e564be14414448
elisamarenzi <37068844+elisamarenzi@users.noreply.github.com>
2019-11-15 12:22:01 +0100

    setup file to build the package

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='dbbs-models',
+     version='0.1',
+     author="Robin De Schepper, Alice Geminiani, Elisa Marenzi, Stefano Casali, Alberto Antonietti, Claudia Casselato, Egidio D'Angelo",
+     author_email="robingilbert.deschepper@unipv.it",
+     description="Collection of neuron models for the NEURON simulator",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/dbbs-lab/models",
+     license='GPLv3',
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "Operating System :: OS Independent",
+     ],
+ )

d9a1b109f87de27360c6cf6b7e4d2f7d998bede7
Robin De Schepper <robin.deschepper93@gmail.com>
2019-12-02 16:58:42 +0100

    install requires nrn-glia and dbbs-mod-collection for the management of mod files

--- setup.py
+++ setup.py
@@ -18,4 +18,8 @@ setuptools.setup(
          "Programming Language :: Python :: 3",
          "Operating System :: OS Independent",
      ],
+     install_requires=[
+        "nrn-glia",
+        "dbbs-mod-collection"
+     ]
  )

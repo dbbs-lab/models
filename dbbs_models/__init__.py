af57151feef737f59207959f0dfd4f0f621182a3
Robin De Schepper <robin.deschepper93@gmail.com>
2020-02-20 11:48:21 +0100

    0.3.1 - optimized granule cell

--- dbbs_models/__init__.py
+++ dbbs_models/__init__.py
@@ -4,5 +4,5 @@ from .golgi_cell_models import GolgiCell
 from .purkinje_cell_models import PurkinjeCell
 import arborize, os, sys
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 arborize.add_directory(os.path.abspath(os.path.join(os.path.dirname(__file__), "morphologies")))

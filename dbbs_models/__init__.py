8f1c722c2ce9b0ba8dc76fb4b0faf3e22fa45d27
Robin De Schepper <robin.deschepper93@gmail.com>
2020-03-06 19:35:08 +0100

    0.4 basket cell

--- dbbs_models/__init__.py
+++ dbbs_models/__init__.py
@@ -5,5 +5,5 @@ from .golgi_cell_models import GolgiCell
 from .purkinje_cell_models import PurkinjeCell
 import arborize, os, sys
 
-__version__ = "0.3.3"
+__version__ = "0.4.0"
 arborize.add_directory(os.path.abspath(os.path.join(os.path.dirname(__file__), "morphologies")))

82b2f5150e717e85ff22815235762d03345c5b6d
Robin De Schepper <robin.deschepper93@gmail.com>
2020-03-06 18:27:11 +0100

    Basket cell model
    import basket cell
    
    fixed name
    
    change name basket morphology
    
    change name basket morphology

--- dbbs_models/__init__.py
+++ dbbs_models/__init__.py
@@ -1,5 +1,6 @@
 from .granule_cell_models import GranuleCell
 from .stellate_cell_models import StellateCell
+from .basket_cell_models import BasketCell
 from .golgi_cell_models import GolgiCell
 from .purkinje_cell_models import PurkinjeCell
 import arborize, os, sys

0639e26fe6e30ea31e3710d756cf77330665fd41
Robin De Schepper <robin.deschepper93@gmail.com>
2020-10-10 22:16:12 +0200

    ndsb operational

--- devops/travis/upload_artifacts.py
+++ devops/travis/upload_artifacts.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
-import ndsb, os
+import ndsb, os, sys, pathlib
 
+sys.path.insert(1, str(pathlib.Path(os.getcwd()) / "tests"))
+import protocols
 pickles = ndsb.thaw()
-beam = ndsb.artificer()
-beam.fire(os.getenv("NDSB_VAULT_URL"), os.getenv("NDSB_VAULT_KEY"), debug=True)
+try:
+    beam = ndsb.artificer(pickles)
+    response = beam.fire(os.getenv("NDSB_VAULT_URL"), os.getenv("NDSB_VAULT_KEY"))
+    beam_id = response["id"]
+
+beam_path = pathlib.Path(os.getenv("NDSB_VAULT_URL")).parent
+trace_path = beam_path / f"emit/{beam_id}/0/trace.html"
--- tests/protocols/_artifacts.py
+++ tests/protocols/_artifacts.py
@@ -17,13 +17,16 @@ class VoltageTrace(ndsb.Data):
                 **self.kwargs
             }
         })
+        kwargs_str = " (" + ";".join(f"{k}={v}" for k, v in self.kwargs.items()) + ")"
+        fig = go.Figure(data=go.Scatter(x=self.t, y=self.v), layout=dict(
+            xaxis_title="Time (ms)",
+            yaxis_title="Voltage (mV)",
+            title_text=self.cell_name + " " + self.protocol + kwargs_str
+        ))
         with artifact.open("trace.html") as f:
-            kwargs_str = " (" + ";".join(f"{k}={v}" for k, v in self.kwargs.items()) + ")"
-            go.Figure(data=go.Scatter(x=self.t, y=self.v), layout=dict(
-                xaxis_title="Time (ms)",
-                yaxis_title="Voltage (mV)",
-                title_text=self.cell_name + " " + self.protocol + kwargs_str
-            )).write_html(f, include_plotlyjs='cdn')
-        with artifact.open("trace.hdf5") as f:
-            h5 = h5py.File(f)
-            h5.create_dataset("data", data=[t, v])
+            fig.write_html(f, include_plotlyjs='cdn')
+        with artifact.open("trace.png", "wb") as f:
+            f.write(fig.to_image(format="png", width=1280, height=720))
+        # with artifact.open("trace.hdf5", "w") as f:
+        #     h5 = h5py.File(f, "w")
+        #     h5.create_dataset("data", data=[self.t, self.v])

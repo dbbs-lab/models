ec7287175fdf673c4cd205e883182e0e85405255
Robin De Schepper <robin.deschepper93@gmail.com>
2020-10-10 23:27:56 +0200

    switched to NEURON from pip in requirements.txt

--- devops/travis/install.sh
+++ devops/travis/install.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env sh
-wget https://neuron.yale.edu/ftp/neuron/versions/v7.7/nrn-7.7.x86_64-linux.deb
-sudo dpkg -i nrn-7.7.x86_64-linux.deb
-export PYTHONPATH=/usr/local/nrn/lib/python:$PYTHONPATH
-sudo apt-get install -y python3-rtree build-essential cmake cython libgsl-dev libltdl-dev libncurses-dev libreadline-dev openmpi-bin libopenmpi-dev
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
 pip3 install coverage

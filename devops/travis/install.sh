77ef1ecc6e2521cb63cb8651615e4e99520022a1
Robin De Schepper <robin.deschepper93@gmail.com>
2020-10-11 10:58:53 +0200

    Removed (hopefully) superfluous libraries

--- devops/travis/install.sh
+++ devops/travis/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 sudo apt-get update
-sudo apt-get install -y python3-rtree build-essential cmake cython libgsl-dev libltdl-dev libncurses-dev libreadline-dev openmpi-bin libopenmpi-dev
+sudo apt-get install -y openmpi-bin libopenmpi-dev
 python --version
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt

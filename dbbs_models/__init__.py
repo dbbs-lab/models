fef0e9de631e3e733697f76d2eba33778e9b88df
Robin De Schepper <robin.deschepper93@gmail.com>
2021-09-02 10:55:52 +0200

    removed new

--- dbbs_models/__init__.py
+++ dbbs_models/__init__.py
@@ -3,7 +3,7 @@ __version__ = "1.5.0.dev1"
 from .granule_cell_models import GranuleCell
 from .stellate_cell_models import StellateCell
 from .basket_cell_models import BasketCell
-from .golgi_cell_models import GolgiCell, GolgiCellNew
+from .golgi_cell_models import GolgiCell
 from .purkinje_cell_models import PurkinjeCell
 
 from .test import quick_test, quick_plot

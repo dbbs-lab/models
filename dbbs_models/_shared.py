d58a7a88f04f4acca0431d739a73690504010f9b
Robin De Schepper <robin.deschepper93@gmail.com>
2022-06-09 14:37:33 +0200

    wip

--- dbbs_models/_shared.py
+++ dbbs_models/_shared.py
@@ -4,12 +4,9 @@ import glia
 import sys
 import os
 
-_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "morphologies"))
 
-
-class DbbsNeuronModel(arborize.NeuronModel, abstract=True):
+class DbbsNeuronModel(arborize.NeuronModel):
     glia_package = "dbbs_mod_collection"
-    morphology_directory = _dir
 
     @classmethod
     def make_catalogue(cls):

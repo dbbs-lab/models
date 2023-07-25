9e1a9015a0b26c2bdb1512316b31cc872bf90663
Robin De Schepper <robin.deschepper93@gmail.com>
2023-07-25 16:29:00 +0200

    add cell builders for each model

--- dbbs_models/__init__.py
+++ dbbs_models/__init__.py
@@ -7,3 +7,39 @@ from .golgi_cell_models import GolgiCellModel
 from .purkinje_cell_models import PurkinjeCellModel
 
 from .test import quick_test, quick_plot
+
+
+def build_granule_cell():
+    return _build(GranuleCellModel, "GranuleCell.swc")
+
+
+def build_golgi_cell():
+    return _build(GolgiCellModel, "GolgiCell.swc")
+
+
+def build_purkinje_cell():
+    return _build(PurkinjeCellModel, "PurkinjeCell.swc")
+
+
+def build_basket_cell():
+    return _build(BasketCellModel, "BasketCell.swc")
+
+
+def build_stellate_cell():
+    return _build(StellateCellModel, "StellateCell.swc")
+
+
+def _build(definition, morphology_file):
+    from pathlib import Path
+    from arborize import neuron_build, bsb_schematic
+    from bsb.morphologies import Morphology
+
+    return neuron_build(
+        bsb_schematic(
+            Morphology.from_swc(
+                Path(__file__).parent / "morphologies" / morphology_file,
+                tags=getattr(definition, "swc_tags", None),
+            ),
+            definition,
+        )
+    )
